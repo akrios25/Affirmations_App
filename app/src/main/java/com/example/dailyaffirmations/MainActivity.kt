a553fae0b1684edeabeb7f93fb52bc8d2bb5f3d9
Kevin Toro <ktoro1251@gmail.com>
2023-08-17 18:03:23 -0400

    Recyclerview implemented

--- app/src/main/java/com/example/dailyaffirmations/MainActivity.kt
+++ app/src/main/java/com/example/dailyaffirmations/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.dailyaffirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.dailyaffirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
---
+++ app/src/main/java/com/example/dailyaffirmations/model/Affirmation.kt
@@ -0,0 +1,5 @@
+package com.example.dailyaffirmations.model
+
+data class Affirmation(val stringResourceId: Int) {
+
+}
