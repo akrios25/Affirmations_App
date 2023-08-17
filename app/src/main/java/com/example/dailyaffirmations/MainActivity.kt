9c6edbdfed4963245ed9060dba160ec1558fa75b
Kevin Toro <ktoro1251@gmail.com>
2023-08-17 18:14:39 -0400

    activity adjustment

--- app/src/main/java/com/example/dailyaffirmations/MainActivity.kt
+++ app/src/main/java/com/example/dailyaffirmations/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.dailyaffirmations
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dailyaffirmations.adapter.ItemAdapter
 import com.example.dailyaffirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
 
     }
 }
